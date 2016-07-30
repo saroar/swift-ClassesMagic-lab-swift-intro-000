956f3f41d71562071dec9aa741dc9a838078c9d6
Michael Dippery <michael@monkey-robot.com>
2016-07-29 20:48:05 -0700

    Implement the `Card` class and tests

---
+++ ClassesMagicFun/Card.swift
@@ -0,0 +1,36 @@
+//
+//  Card.swift
+//  ClassesMagicFun
+//
+//  Created by Michael Dippery on 7/29/16.
+//  Copyright © 2016 Gamesmith, LLC. All rights reserved.
+//
+
+import Foundation
+
+class Card {
+    let suit: String
+    let rank: String
+    var label: String {
+        return rank + suit
+    }
+    var value: UInt {
+        switch rank {
+        case "A":
+            return 1
+        case "J", "K", "Q":
+            return 10
+        default:
+            if let value = UInt(rank) {
+                return value
+            } else {
+                return 0
+            }
+        }
+    }
+
+    init(suit: String, rank: String) {
+        self.suit = suit
+        self.rank = rank
+    }
+}
---
+++ ClassesMagicFunTests/CardSpec.swift
@@ -0,0 +1,76 @@
+//
+//  CardSpec.swift
+//  ClassesMagicFun
+//
+//  Created by Michael Dippery on 7/29/16.
+//  Copyright © 2016 Gamesmith, LLC. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import Quick
+import Nimble
+@testable import ClassesMagicFun
+
+class CardSpec: QuickSpec {
+    override func spec() {
+        describe("Card") {
+            let aceOfSpades = Card(suit: "♠︎", rank: "A")
+            let queenOfHearts = Card(suit: "♥︎", rank: "Q")
+            let tenOfDiamonds = Card(suit: "♦︎", rank: "10")
+            let twoOfClubs = Card(suit: "♣︎", rank: "2")
+
+            describe("initializer") {
+                it("should set the suit property") {
+                    expect(aceOfSpades.suit).to(equal("♠︎"))
+                    expect(queenOfHearts.suit).to(equal("♥︎"))
+                    expect(tenOfDiamonds.suit).to(equal("♦︎"))
+                    expect(twoOfClubs.suit).to(equal("♣︎"))
+                }
+
+                it("should set the rank property") {
+                    expect(aceOfSpades.rank).to(equal("A"))
+                    expect(queenOfHearts.rank).to(equal("Q"))
+                    expect(tenOfDiamonds.rank).to(equal("10"))
+                    expect(twoOfClubs.rank).to(equal("2"))
+                }
+            }
+
+            describe("suit") {
+                it("should be the suit") {
+                    expect(aceOfSpades.suit).to(equal("♠︎"))
+                    expect(queenOfHearts.suit).to(equal("♥︎"))
+                    expect(tenOfDiamonds.suit).to(equal("♦︎"))
+                    expect(twoOfClubs.suit).to(equal("♣︎"))
+                }
+            }
+
+            describe("rank") {
+                it("should be the rank") {
+                    expect(aceOfSpades.rank).to(equal("A"))
+                    expect(queenOfHearts.rank).to(equal("Q"))
+                    expect(tenOfDiamonds.rank).to(equal("10"))
+                    expect(twoOfClubs.rank).to(equal("2"))
+                }
+            }
+
+            describe("label") {
+                it("should be the rank and the suit") {
+                    expect(aceOfSpades.label).to(equal("A♠︎"))
+                    expect(queenOfHearts.label).to(equal("Q♥︎"))
+                    expect(tenOfDiamonds.label).to(equal("10♦︎"))
+                    expect(twoOfClubs.label).to(equal("2♣︎"))
+                }
+            }
+
+            describe("value") {
+                it("should return the blackjack value") {
+                    expect(aceOfSpades.value).to(equal(1))
+                    expect(queenOfHearts.value).to(equal(10))
+                    expect(tenOfDiamonds.value).to(equal(10))
+                    expect(twoOfClubs.value).to(equal(2))
+                }
+            }
+        }
+    }
+}
