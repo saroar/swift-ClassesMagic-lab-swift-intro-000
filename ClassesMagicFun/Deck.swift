a71717235e67943a32950d82e17d25d443424b98
Michael Dippery <michael@monkey-robot.com>
2016-07-29 22:30:20 -0700

    Implement Deck class with tests, sans shuffling

---
+++ ClassesMagicFun/Deck.swift
@@ -0,0 +1,37 @@
+//
+//  Deck.swift
+//  ClassesMagicFun
+//
+//  Created by Michael Dippery on 7/29/16.
+//  Copyright © 2016 Gamesmith, LLC. All rights reserved.
+//
+
+import Foundation
+
+class Deck {
+    var cards: [Card]
+    var nextCard: Int
+
+    init() {
+        nextCard = 0
+        cards = []
+        for suit in ["♠️", "♣️", "♥️", "♦️"] {
+            for rank in ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"] {
+                let card = Card(suit: suit, rank: rank)
+                cards.append(card)
+            }
+        }
+        shuffle()
+    }
+
+    func drawCard() -> Card? {
+        guard nextCard < cards.count else { return nil }
+        let card = cards[nextCard]
+        nextCard += 1
+        return card
+    }
+
+    func shuffle() {
+        nextCard = 0
+    }
+}
